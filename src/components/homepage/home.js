4a5ec1001a4c89d7539b62b16ebfe6fe717b383d
Luc-Frédéric Langis <luc-frederic.langis@ekino.com>
2018-06-18 18:06:13 +0200

    feat(home): add data home with graphql

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Page from '../commons/page';
 import Avatar from '../avatar/avatar';
 import Fade from '../animations/fade';
+import Text from '../commons/text';
+import AppContext from '../../context/appContext';
 
 /**
  * Like in every video game, we land on the famous "press start screen"
@@ -12,6 +14,18 @@ const Home = () => (
       <Fade duration={1}>
         <Avatar />
       </Fade>
+      <AppContext.Consumer>
+        {({ author, description }) => (
+          <React.Fragment>
+            <Fade duration={2}>
+              <Text style={{ textAlign: 'center' }}>{author}</Text>
+            </Fade>
+            <Fade duration={3}>
+              <Text style={{ textAlign: 'center' }}>{description}</Text>
+            </Fade>
+          </React.Fragment>
+        )}
+      </AppContext.Consumer>
     </div>
   </Page>
 );
