45848a444d119c165856773105cf987b019d48e8
Luc-Frédéric Langis <luc-frederic.langis@ekino.com>
2018-07-19 17:23:48 +0200

    feat(content): add breakpoint

--- src/components/commons/content.js
+++ src/components/commons/content.js
@@ -2,20 +2,23 @@ import styled from 'styled-components';
 
 const Content = styled.div`
   background-color: #ffffff;
-  max-width: 980px;
-  margin: 64px auto;
-  padding: 16px 32px;
   display: flex;
   border-radius: 8px;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
+  justify-content: flex-start;
+  margin: 32px 8px;
+  padding: 8px 12px;
 
-  @media (max-width: 700px) {
-    justify-content: flex-start;
-    align-items: initial;
-    margin: 32px 8px;
-    padding: 8px 12px;
+  @media (min-width: 700px) {
+    align-items: center;
+    justify-content: center;
+    margin: 64px auto;
+    padding: 16px 32px;
+    max-width: 650px;
+  }
+
+  @media (min-width: 1150px) {
+    max-width: 980px;
   }
 `;
 
